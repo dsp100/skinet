3ed41023d81dd8e1bcfef343b61a113567afbe10
David Pope <dpope100@gmail.com>
2021-04-04 13:14:01 +0100

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 
+using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -27,6 +28,8 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
@@ -51,7 +54,7 @@ namespace API
 
             //routes to end points from middleware
             app.UseRouting();
-
+            app.UseStaticFiles();
             app.UseAuthorization();
 
             
