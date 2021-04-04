0ce5cca320df8aa4e371bb836202dfe65164b026
David Pope <dpope100@gmail.com>
2021-04-04 16:55:36 +0100

    End of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,8 @@
 
+
+using API.Extensions;
 using API.Helpers;
-using Core.Interfaces;
+using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,7 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+
 
 namespace API
 {
@@ -27,26 +29,29 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddApplicationServices();
+            services.AddSwaggerDocumention();
+         
+
+          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. this is for middleware
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+            
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+             
+                
             }
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
 
 
             //reroutes to https
@@ -56,6 +61,8 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
             app.UseAuthorization();
+
+            app.UseSwaggerDocuments();
 
             
             app.UseEndpoints(endpoints =>
