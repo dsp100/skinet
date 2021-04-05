eda2afa2d7017651d39fe91af05b000b96177dc3
David Pope <dpope100@gmail.com>
2021-04-06 00:05:54 +0100

    End of Section 7

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -16,6 +16,8 @@ namespace Infrastructure.Data
             _context = context;
         }
 
+
+
         public async Task<T> GetByIdAsync(int id)
         {
             return await _context.Set<T>().FindAsync(id);
@@ -35,6 +37,14 @@ namespace Infrastructure.Data
         {
             return await ApplySpecification(spec).ToListAsync();
         }
+
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
+
+
 
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
