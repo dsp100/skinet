9367a7191273bd46b3d973fa73230aea7a26a0a4
David Pope <dpope100@gmail.com>
2021-04-05 11:11:47 +0100

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,31 +24,38 @@ namespace API
             _config = config;
         }
 
-        
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumention();
-         
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
+            });
 
-          
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. this is for middleware
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-            
+
             if (env.IsDevelopment())
             {
-             
-                
+
+
             }
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
@@ -60,11 +67,14 @@ namespace API
             //routes to end points from middleware
             app.UseRouting();
             app.UseStaticFiles();
+            
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
 
             app.UseSwaggerDocuments();
 
-            
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
