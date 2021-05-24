beed4e4d55205396197ea466e45648b74d744718
David Pope <dpope100@gmail.com>
2021-05-24 23:39:49 +0100

    End of section 17

--- client/src/app/core/interceptors/loading.interceptors.ts
+++ client/src/app/core/interceptors/loading.interceptors.ts
@@ -1,5 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { registerEscClick } from "ngx-bootstrap/utils";
 import { Observable } from "rxjs";
 import { delay, finalize } from "rxjs/Operators";
 import { BusyService } from "../services/busy.service";
@@ -9,7 +10,8 @@ export class LoadingInterceptor implements HttpInterceptor{
     constructor(private busyService: BusyService ){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        this.busyService.busy();
+        if(!req.url.includes('emailexists')){
+        this.busyService.busy();}
         return next.handle(req).pipe(
                 delay(1000),
                 finalize(()=> {
