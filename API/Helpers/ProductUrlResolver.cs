48dbfcb5e9aee1e5d8bb4a887ae4ddeeca99604f
David Pope <dpope100@gmail.com>
2021-05-31 10:50:45 +0100

    End of section 18

--- API/Helpers/ProductUrlResolver.cs
+++ API/Helpers/ProductUrlResolver.cs
@@ -25,6 +25,7 @@ namespace API.Helpers
                 return null;
         }
 
+        
 
 
     }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -4,40 +4,59 @@ using Microsoft.EntityFrameworkCore;
 using Core.Entities;
 using System.Reflection;
 using System.Linq;
+using Core.Entities.OrderAggregate;
+using System;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Data
 {
     public class StoreContext : DbContext
     {
-        public StoreContext( DbContextOptions<StoreContext> options) : base(options)
+     
+
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
         {
         }
-            public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
 
-            public DbSet<ProductBrand> ProductBrands {get; set;}
+        public DbSet<Order> Orders { get; set; }
 
-            public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<OrderItem> OrderItems{ get; set; }
 
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
+        public DbSet<DeliveryMethod> DeliveryMethods{ get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
             {
-                    base.OnModelCreating(modelBuilder);
-                    modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType
+                    == typeof(decimal));
 
-                    if(Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+                    var dateTimeProperties = entityType.ClrType.GetProperties().Where(p =>p.PropertyType ==typeof(DateTimeOffset)) ;
+
+                    foreach (var property in properties)
                     {
-                            foreach(var entityType in modelBuilder.Model.GetEntityTypes())
-                            {
-                                var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType
-                                == typeof(decimal));
-                                
-                                foreach(var property in properties)
-                                {
-                                    modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
-                                }
-                            }
-                        
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
                     }
+
+                    foreach (var property in dateTimeProperties)
+                    {
+                        object p = modelBuilder.Entity(entityType.Name).Property(property.Name)
+                        .HasConversion(new DateTimeOffsetToBinaryConverter());
+                    }
+                }
+
             }
+        }
 
     }
 }
