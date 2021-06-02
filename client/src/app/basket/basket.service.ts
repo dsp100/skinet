47612639e2ea08bc0e4e1a8884d374b6a0b35267
David Pope <dpope100@gmail.com>
2021-06-02 20:34:07 +0100

    End of Section 19

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/Operators';
 import { environment } from 'src/environments/environment';
 import { Basket, IBasket, IBasketItem, IBasketTotals } from '../shared/models/basket';
+import { IDeliveryMethod } from '../shared/models/deliveryMethod';
 import { IProduct } from '../shared/models/product';
 
 
@@ -16,8 +17,14 @@ private basketSource = new BehaviorSubject<IBasket>(null);
 basket$ = this.basketSource.asObservable();
 private basketTotalSource = new BehaviorSubject<IBasketTotals>(null);
 basketTotal$ = this.basketTotalSource.asObservable();
+shipping = 0;
 
   constructor(private http: HttpClient) { }
+
+  setShippingPrice(deliveryMethod: IDeliveryMethod){
+    this.shipping = deliveryMethod.price;
+    this.calculateTotals();
+  }
 
   getBasket(id: string){
     return this.http.get(this.baseUrl + 'basket?id='+ id)
@@ -76,6 +83,14 @@ basketTotal$ = this.basketTotalSource.asObservable();
               }     
              }
       }
+
+        deleteLocalBasket(id: string){
+          this.basketSource.next(null);
+          this.basketTotalSource.next(null);
+          localStorage.removeItem('basket_id');
+        }
+
+
         deleteBasket(basket: IBasket) {
    return this.http.delete(this.baseUrl + 'basket?id=' + basket.id).subscribe(() => {
    this.basketSource.next(null);
@@ -90,7 +105,7 @@ basketTotal$ = this.basketTotalSource.asObservable();
 
     private calculateTotals(){
       const basket = this.getCurrentBasketValue();
-      const shipping = 0;
+      const shipping = this.shipping;
       const subtotal = basket.items.reduce((a, b) => (b.price * b.quantity) + a, 0);
       const total = subtotal + shipping;
       this.basketTotalSource.next({shipping, total, subtotal  })
